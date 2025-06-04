81b08387f3523af10d8a9cd872cac91676da888d
vikashakhlai <vikto.0222@gmail.com>
2025-06-04 16:44:15 +0300

    fix URL

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
-// export const API_URL = `https://garbage-collection-backend.onrender.com/api`;
-export const API_URL = `http://localhost:4200/api`;
+export const API_URL = `https://garbage-collection-backend.onrender.com/api`;
+// export const API_URL = `http://localhost:4200/api`;
 // export const API_URL = `${process.env.APP_URL}/api`;
 
 export const getAuthUrl = (string: string) => `/auth/${string}`;
